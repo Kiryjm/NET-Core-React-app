1c630ba14d59b85b2e88b773db030dc07d6f7a0f
Kiryjm <kiryjm@gmail.com>
2021-03-20 06:29:54 +0300

    Delete activities function and button were added.

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -47,6 +47,10 @@ function App() {
     setEditMode(false);
   }
 
+  function handleDeleteActivity(id: string) {
+    setActivities([...activities.filter(x => x.id !== id)])
+  }
+
   return (
     <>
       <NavBar openForm={handleFormOpen} />
@@ -60,6 +64,7 @@ function App() {
           openForm={handleFormOpen}
           closeForm={handleFormClose}
           createOrEdit={handleCreateOfEditActivity}
+          deleteActivity = {handleDeleteActivity}
         />
       </Container>
     </>
