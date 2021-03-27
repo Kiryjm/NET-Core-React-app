468bc431432303766b9b307d32022764db880c3b
Kiryjm <kiryjm@gmail.com>
2021-03-27 03:22:50 +0300

    Creating, updating, deleting activities functions were modified and moved to activityStore, activityForm and activityList were modified to be observable.

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,10 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { Container} from 'semantic-ui-react';
-import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-import {v4 as uuid} from 'uuid';
-import agent from '../api/agent';
 import LoadingComponent from './LoadingComponent';
 import { useStore } from '../stores/store';
 import { observer } from 'mobx-react-lite';
@@ -17,11 +14,6 @@ function App() {
   // useState allows to store state inside component
   // activities stores a component state
   // function setActivities used for setting state
-  const [activities, setActivities] = useState<Activity[]>([]);
-  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
-  const [editMode, setEditMode] = useState(false);
-  const[submitting, setSubmitting] = useState(false);
-
 
   // useEffect allows to use side effect when component initializes
   // adding array of dependencies will ensure useEffect will not execute endlessly
@@ -29,48 +21,13 @@ function App() {
     activityStore.loadActivities();
   }, [activityStore])
 
- 
-
-  function handleCreateOrEditActivity(activity: Activity) {
-    setSubmitting(true);
-    if (activity.id) {
-      agent.Activities.update(activity).then(() => {
-        setActivities([...activities.filter(x => x.id !== activity.id), activity])
-        setSelectedActivity(activity);
-        setEditMode(false);
-        setSubmitting(false);
-      })
-    } else {
-      activity.id = uuid();
-      agent.Activities.create(activity).then(() => {
-        setActivities([...activities, activity])
-        setSelectedActivity(activity);
-        setEditMode(false);
-        setSubmitting(false);
-      })
-    }
-  }
-
-  function handleDeleteActivity(id: string) {
-    setSubmitting(true);
-    agent.Activities.delete(id).then(() => {
-      setActivities([...activities.filter(x => x.id !== id)])
-      setSubmitting(false);
-    })
-  }
-
   if (activityStore.loadingInitial) return <LoadingComponent content='Loading app' />
 
   return (
     <>
       <NavBar />
       <Container style={{marginTop: '7em'}}>
-        <ActivityDashboard
-          activities={activityStore.activities}
-          createOrEdit={handleCreateOrEditActivity}
-          deleteActivity={handleDeleteActivity}
-          submitting={submitting}
-        />
+        <ActivityDashboard />
       </Container>
     </>
   );
